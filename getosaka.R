7bc12fbf5e3db4307356a55bf09ca45708e92401
tomoya <tomfook@gmail.com>
2020-06-26 23:40:26 +0900

    Osaka data source changed from excel to json of codeforosaka

--- getosaka.R
+++ getosaka.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(lubridate)
 library(httr)
-library(readxl)
+library(jsonlite)
 
 
 pref <- "osaka"
@@ -10,24 +10,24 @@ file_latest <- paste0("infections_", pref, ".csv")
 file_record <- paste0("infections_record_", pref, ".csv") 
 source("secret.R") #slack_webhookurl
 
-osaka_url <- "http://www.pref.osaka.lg.jp/attach/23711/00346644/youseisyajyouhou.xlsx"
+osaka_url <- "https://raw.githubusercontent.com/codeforosaka/covid19/development/data/data.json"
 url_guide <- "http://www.pref.osaka.lg.jp/hodo/index.php?site=fumin"
 
-GET(osaka_url, write_disk(tf <- tempfile(fileext = ".xlsx")))                 
-infections <- read_excel(
-		tf,
-		skip = 1,
-		col_types = c("text", "date", "text", "text", "text", "date", "text")
-              ) %>%
-              mutate(報道提供日 = as_date(報道提供日), 発症日 = as_date(発症日))
+infections <- GET(osaka_url) %>%
+  content %>%
+  parse_json %>%
+  .[["patients"]] %>%
+  .[["data"]] %>%
+  bind_rows %>%
+  mutate(No = as.character(No), date = ymd(date))
 
 if (any(dir() %in% file_latest)){
-  old_infections <- read_csv(file_latest, col_types = "cDcccDc")
+  old_infections <- read_csv(file_latest, col_types = "cccccccD")
 }else{
   old_infections <- infections
 }
 
-diff <- infections %>% anti_join(old_infections, by = "番号") 
+diff <- infections %>% anti_join(old_infections, by = "No") 
 
 growth <- nrow(infections) - nrow(old_infections) 
 check_health <- growth >= 0
@@ -37,7 +37,7 @@ if(check_health){
     for(i in seq(to = nrow(diff))){
       text <- paste0(
 		     "大阪府発表\n",
-		     "報道提供日：", diff[i,]$報道提供日, " 年代:", diff[i,]$年代, " 性別：", diff[i,]$性別, " 居住地：", diff[i,]$居住地, "\n",
+		     "報道日：", diff[i,]$date, " 年代:", diff[i,]$年代, " 性別：", diff[i,]$性別, " 居住地：", diff[i,]$居住地, "\n",
 		     url_guide
       )
       POST(url = slack_webhookurl, encode = "json", body = list(text = text))
