ce27ae9f06fba681dc0a13850e040a279356d397
tomfook <tomfook@gmail.com>
2022-03-05 02:46:45 +0900

    data update

---
+++ secret.R
@@ -0,0 +1,6 @@
+slack_webhookurl <- "https://hooks.slack.com/services/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" #fukumoto
+slack_webhookurl2 <- "https://hooks.slack.com/services/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" #haranishi
+
+line_token <- "fUWiRyOhlB5QNUyYZijqtx9PFFygOyzIhz0WXpx8ufq"
+
+
